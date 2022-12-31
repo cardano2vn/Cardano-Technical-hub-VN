e4590a5992f3135d1687129ae3582f9638fa7337
Nguyen Anh Tien <34856010+tienna@users.noreply.github.com>
2022-12-31 14:54:14 +0700

    Add files via upload

---
+++ NFT Metada.sh
@@ -0,0 +1,249 @@
+{
+    "721": {
+        "2ada81ed42a4e591e3b93ef02552b09f32b770ed7f05aabcb55170b8": {
+            "NFT-1": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-1",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-2": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-2",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },		
+            "NFT-3": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-3",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-4": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-4",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-5": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-5",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-6": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-6",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-7": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-7",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-8": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-8",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-9": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-9",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-10": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-10",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-11": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-11",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-12": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-12",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-13": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-13",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-14": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-14",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-15": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-15",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-16": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-16",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-17": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-17",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-18": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-18",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-19": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-19",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-20": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-20",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-21": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-21",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-22": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-22",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-23": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-23",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-24": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-24",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-25": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-25",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },
+            "NFT-26": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-26",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },			
+            "NFT-27": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-27",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },			
+            "NFT-28": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-28",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },	
+            "NFT-29": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-29",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            },		
+            "NFT-30": {
+                "description": "This is an NFT certificate to record your Participation.",
+				"name": "WMTViet-Cardano2vn NFT-30",
+				"website": "cardano2vn.io",
+				"Telegram": "t.me/wmtviet",
+				"Telegram": "t.me/cardano2vn",
+				"image": "ipfs://QmXisLBU9BRj2N4ujHiME5WQP2PeqbvkxhZQ8mYutFny5g"
+            }
+        }
+    }
+}
+
+
+
